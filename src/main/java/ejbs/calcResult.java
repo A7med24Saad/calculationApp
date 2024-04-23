61f3e3dc11c475f28ce17911d02da3b8008a2bd1
A7med24Saad <102108617+A7med24Saad@users.noreply.github.com>
2024-04-23 22:48:44 +0200

    Initial commit

---
+++ src/main/java/ejbs/calcResult.java
@@ -0,0 +1,15 @@
+package ejbs;
+
+public class calcResult {
+	private double Result;
+	public calcResult(double r){
+		Result =r;
+	}
+	
+	public double getResult() {
+		return Result;
+	}
+	public void setResult(double r) {
+		Result =r;
+	}
+}
